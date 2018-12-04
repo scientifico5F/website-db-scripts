b4d2563dbd6e200582cd2e9239def47e752c7ba7
studente <studente@liceospalla.local>
2018-12-04 10:51:08 +0100

    add attachements

--- fillTables.sql
+++ fillTables.sql
@@ -65,7 +65,182 @@ VALUES (
   "http://olimpiadi.dm.unibo.it/wp-content/uploads/2015/06/O-095.jpg",
   "image/jpeg",
   "gara di Cesenatico dei nostri amici"
+),
+(
+  2,
+  "https://upload.wikimedia.org/wikipedia/commons/2/23/Lake_mapourika_NZ.jpeg",
+  "image/jpeg",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  3,
+  "https://jpeg.org/images/jpeg2000-home.jpg",
+  "image/jpeg",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  4,
+  "https://jpeg.org/images/jpegsystems-home.jpg",
+  "image/jpeg",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  5,
+  "https://docs.gimp.org/2.10/it/images/tutorials/quickie-jpeg-100.jpg",
+  "image/jpeg",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  6,
+  "https://docs.gimp.org/2.2/images/tutorials/quickie-jpeg-example.jpg",
+  "image/jpeg",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  7,
+  "https://jpeg.org/images/jpegls-home.jpg",
+  "image/jpeg",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  8,
+  "https://flif.info/example-images/fish-1.jpg",
+  "image/jpeg",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  9,
+  "http://photoartinc.com/wp-content/uploads/2018/02/jpeg-photos-4.jpg",
+  "image/jpeg",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  10,
+  "http://howtoscan.ca/assets/images/24-bit-jpeg-scan.jpg",
+  "image/jpeg",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  11,
+  "https://upload.wikimedia.org/wikipedia/commons/5/57/PT05_ubt.jpeg",
+  "image/jpeg",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  12,
+  "https://upload.wikimedia.org/wikipedia/commons/2/2b/Ruby-LowCompression-Tiny.jpg",
+  "image/jpeg",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  13,
+  "https://www.guidatorino.com/wp-content/uploads/2018/08/natale-venaria-reale-2018.jpg",
+  "image/jpeg",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  14,
+  "http://www.meteoweb.eu/wp-content/uploads/2016/12/buon-natale5.jpg",
+  "image/jpeg",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  15,
+  "http://www.ntr24.tv/wp-content/uploads/2018/12/regali_natale.jpg",
+  "image/jpeg",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  16,
+  "https://mr.comingsoon.it/imgdb/PrimoPiano/73724_ppl.jpg",
+  "image/jpeg",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  17,
+  "http://www.superedo.it/sfondi/sfondi/Feste/Natale/natale_007.jpg",
+  "image/jpeg",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  18,
+  "http://www.class24.it/wp-content/uploads/2015/12/alberi_di_natale_00006.jpeg",
+  "image/jpeg",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  19,
+  "https://www.lastampa.it/rf/image_lowres/Pub/p4/2017/12/23/Societa/Foto/RitagliWeb/natale-kwUF-U11011614325399v0F-1024x576%40LaStampa.it.JPG",
+  "image/jpeg",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  20,
+  "https://www.viridea.it/wp-content/uploads/2016/09/promo_natale_21.jpg",
+  "image/jpeg",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  21,
+  "https://www.repstatic.it/content/periodici/img/d/2017/11/19/113242914-25802045-dadd-4050-97c3-d8ef3c11df85.jpg",
+  "image/jpeg",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  22,
+  "http://olimpiadi.dm.unibo.it/wp-content/uploads/2015/06/O-095.jpg",
+  "image/jpeg",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  23,
+  "https://www.latuaserata.it/wp-content/uploads/2017/10/dj-per-feste-18-anni.jpg",
+  "image/jpeg",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  24,
+  "http://www.lamorellaricevimenti.it/wp-content/uploads/2016/06/festa-di-compleanno.jpg",
+  "image/jpeg",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  25,
+  "http://images.performgroup.com/di/library/sporting_news/97/15/bucks-header_i5naw0191e0q14tgazuu57v9r.jpg?t=-281734808&w=960&quality=70",
+  "image/jpeg",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  26,
+  "http://www.letteratura-italiana.com/pdf/divina%20commedia/04%20Paradiso.pdf",
+  "application/pdf",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  27,
+  "http://www.letteratura-italiana.com/pdf/divina%20commedia/03%20Purgatorio.pdf",
+  "application/pdf",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  28,
+  "http://www.scuoleasso.gov.it/inclusione/wp-content/uploads/2014/10/Gopvanni-Boccaccio-Vita-Opere-Decameron.pdf",
+  "application/pdf",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  29,
+  "https://www.pearson.it/letteraturapuntoit/contents/files/manz_sintesi.pdf",
+  "application/pdf",
+  "gara di Cesenatico dei nostri amici"
+),
+(
+  30,
+  "https://www.victoria.ac.nz/lals/about/staff/publications/paul-nation/Prince-Adapted2.pdf",
+  "application/pdf",
+  "gara di Cesenatico dei nostri amici"
 );
+
 
 INSERT INTO ContentsCategoriesTh
 VALUES (
